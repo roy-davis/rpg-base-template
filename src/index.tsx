e529ba5e1bfa4c80644d1e31d442a57a622f3c0f
Roy Davis <>
2020-08-24 12:16:31 +1200

    Fixing layout and adding authprovider decorator.

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,26 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
 import {BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import { Auth0Provider } from "@auth0/auth0-react";
-import './Assets/Styles/main.css';
-import { createStore, applyMiddleware } from 'redux';
+// redux
 import { Provider } from 'react-redux';
 import store from './Redux/Store/store'
+// auth
+import AuthProvider from './AuthProvider';
+// app
+import Routes from './Routes';
+import './Assets/Styles/main.css';
 
 ReactDOM.render(
   <Provider store={store}>
-  <Auth0Provider
-    domain="virtualfarm.au.auth0.com"
-    clientId="h7cl0U73pFw0Pcmsw1ju3IEd3jraWgna"
-    redirectUri={window.location.origin}
-  >
-    <React.StrictMode>
-      <Router>
+    <Router>
+      <AuthProvider>
         <Routes />
-      </Router>
-    </React.StrictMode>
-  </Auth0Provider>
+      </AuthProvider>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
